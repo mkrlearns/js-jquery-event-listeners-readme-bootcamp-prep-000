3842b0260e756dae0523745f505b52144a2dc9d1
ryan m <contact@ryanmeek.com>
2019-01-06 01:38:54 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,31 @@
-//define functions here
-
+function getIt() {
+  $('p').on('click', function() {
+    alert('Hey!')
+  });
+}
+function frameIt() {
+  $(document).on('load', function() {
+    $('img').addClass('tasty');
+    $('img').css({"border-color":"red",
+                  "border-width":"1px",
+                  "border-style":"solid"});
+  });
+}
+function pressIt() {
+  $(document).on('keydown', function(key) {
+    if(key.which == 71){
+      alert('GG, You hit "G", G!');
+    }
+  });
+}
+function submitIt() {
+  $("form").on("submit", function() {
+    alert('Your form is going to be submitted now.')
+  });
+}
 $(document).ready(function(){
-
-// call functions here
-
+  getIt();
+  frameIt();
+  pressIt()
+  submitIt();
 });
